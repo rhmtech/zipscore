e2ad98e1a15618dccec7b09e8267a6bc36a576ef
Askar Sagyndyk <superwhykz@gmail.com>
2019-08-07 08:57:32 -0400

    build db

--- main.go
+++ main.go
@@ -2,12 +2,20 @@ package main
 
 import (
 	"bufio"
+	"flag"
 	"log"
 	"os"
 	"strconv"
 	"strings"
 
 	"github.com/dgraph-io/badger"
+)
+
+// Setting version/buildtime
+var (
+	scoreFile string
+	createDb  bool
+	zipcode   string
 )
 
 // check for numeric only
@@ -17,47 +25,59 @@ func isNumeric(s string) bool {
 }
 
 func main() {
+	flag.StringVar(&scoreFile, "c", "", "Score file for somefile.csv.")
+	flag.BoolVar(&createDb, "create", false, "Create BadgerDB")
+	flag.Parse()
 
+	//
 	// initialize BadgerDB
+	//
 	db, err := badger.Open(badger.DefaultOptions("qarikRiskScores"))
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer db.Close()
 
-	// total := 0 // count lines
-	// begin := time.Now()
+	if createDb {
+		var fileName string
+		if len(scoreFile) > 0 {
+			fileName = scoreFile
+		} else {
+			fileName = "scores.csv"
+		}
 
-	f, err := os.OpenFile("scores.csv", os.O_RDONLY, os.ModePerm)
-	if err != nil {
-		log.Fatalf("Open file error: %v", err)
-		return
-	}
-	defer f.Close()
+		// Open file handler
+		f, err := os.OpenFile(fileName, os.O_RDONLY, os.ModePerm)
+		if err != nil {
+			log.Fatalf("Open file error: %v", err)
+			return
+		}
+		defer f.Close()
 
-	sc := bufio.NewScanner(f)
-	for sc.Scan() {
-		line := strings.Split(sc.Text(), ",")
-		// if isNumeric(line[0]) && isNumeric(line[1]) {
-		// 	total++
-		// } else {
-		// 	fmt.Printf("Zipcode: %v, Score: %v\n", line[0], line[1])
-		// }
-		if isNumeric(line[0]) && isNumeric(line[1]) {
-			err := db.Update(func(txn *badger.Txn) error {
-				err := txn.Set([]byte(line[0]), []byte(line[1]))
-				return err
-			})
+		sc := bufio.NewScanner(f)
+		for sc.Scan() {
+			line := strings.Split(sc.Text(), ",")
+			// if isNumeric(line[0]) && isNumeric(line[1]) {
+			// 	total++
+			// } else {
+			// 	fmt.Printf("Zipcode: %v, Score: %v\n", line[0], line[1])
+			// }
+			if isNumeric(line[0]) && isNumeric(line[1]) {
+				err := db.Update(func(txn *badger.Txn) error {
+					err := txn.Set([]byte(line[0]), []byte(line[1]))
+					return err
+				})
 
-			if err != nil {
-				log.Printf("Error committing record: %v", err)
+				if err != nil {
+					log.Printf("Error committing record: %v", err)
+				}
 			}
+
+		}
+		if err := sc.Err(); err != nil {
+			log.Fatalf("Scan error: %v", err)
 		}
 
 	}
-	if err := sc.Err(); err != nil {
-		log.Fatalf("Scan error: %v", err)
-	}
 
-	// log.Printf("Scanned file, time_used: %v, lines=%v\n", time.Since(begin).Seconds(), total)
 }
